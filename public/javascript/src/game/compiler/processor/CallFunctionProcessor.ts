00cb095669587f4a027d3359aec37434047fa7d5
Kainer, Matthias <mail@matthias-kainer.de>
2017-04-30 22:37:57 +0200

    fix CallFunctionProcessor.ts

--- public/javascript/src/game/compiler/processor/CallFunctionProcessor.ts
+++ public/javascript/src/game/compiler/processor/CallFunctionProcessor.ts
@@ -1,9 +1,10 @@
-import { CallFunctionNode } from '../../../ast/availableNodes';
-import { SyntaxNode } from '../../../ast/node';
-import Machine from '../machine';
-import { NodeProcessor } from '../nodeProcessor';
+import { CallFunctionNode } from "../../../ast/availableNodes";
+import { SyntaxNode } from "../../../ast/node";
+import Machine from "../machine";
+import { NodeProcessor } from "../nodeProcessor";
+
 export class CallFunctionProcessor implements NodeProcessor {
-    machine: Machine;
+    private machine: Machine;
 
     public constructor(machine: Machine) {
         this.machine = machine;
@@ -14,12 +15,12 @@ export class CallFunctionProcessor implements NodeProcessor {
     }
 
     public process(node: SyntaxNode): any {
-        let caller = node as CallFunctionNode;
-        let fun = this.machine.getScope().getFunction(this.machine.run(caller.function));
+        const caller = node as CallFunctionNode;
+        const fun = this.machine.getScope().getFunction(this.machine.run(caller.function));
         this.machine.pushScope();
         fun.arguments.forEach(_ => this.machine.run(_));
         caller.arguments.forEach(_ => this.machine.run(_));
-        let result = this.machine.run(fun.operation);
+        const result = this.machine.run(fun.operation);
         this.machine.popScope();
         return result;
     }
