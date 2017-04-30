61b8067716c3493b536f5a46f0937b183f8f2e84
Kainer, Matthias <mail@matthias-kainer.de>
2017-04-30 22:58:20 +0200

    cleaned up SequenceProcessor.ts

--- public/javascript/src/game/compiler/processor/SequenceProcessor.ts
+++ public/javascript/src/game/compiler/processor/SequenceProcessor.ts
@@ -1,22 +1,17 @@
-import Machine from '../machine';
-import { SyntaxNode } from '../../../ast/node';
-import { SequenceNode } from '../../../ast/availableNodes';
-import { NodeProcessor } from '../nodeProcessor';
+import Machine from "../machine";
+import { SyntaxNode } from "../../../ast/node";
+import { SequenceNode } from "../../../ast/availableNodes";
+import { NodeProcessor } from "../nodeProcessor";
 
 export class SequenceProcessor implements NodeProcessor {
-    machine: Machine;
-
-    public constructor(machine: Machine) {
-        this.machine = machine;
-    }
+    public constructor(private machine: Machine) { }
 
     public canHandle(node: SyntaxNode): boolean {
         return node !== null && node.type === "SequenceNode";
     }
 
     public process(node: SyntaxNode): any {
-        let sequence = node as SequenceNode;
-        return sequence.children.map(_ => {
+        return (node as SequenceNode).children.map(_ => {
             return this.machine.run(_);
         });
     }
