4bd7cb94dacd4c1f933c9e708646f518eb57ca73
Kainer, Matthias <mail@matthias-kainer.de>
2017-04-30 22:36:27 +0200

    fix touchspin.tsx

--- public/javascript/src/controls/touchspin.tsx
+++ public/javascript/src/controls/touchspin.tsx
@@ -1,27 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
 
 interface TouchSpinProperties {
-    value : number,
-    onNumberIncrease : () => void;
-    onNumberDecrease : () => void;
-    onNumberChange : (value : number) => void;
+    value: number;
+    onNumberIncrease: () => void;
+    onNumberDecrease: () => void;
+    onNumberChange: (value: number) => void;
 }
 
 export default class TouchSpin extends React.Component<TouchSpinProperties, any> {
-
-    private onNumberChange(value : any) {
-        this.props.onNumberChange(Number.isNaN(value) ? 0 : Number.parseInt(value));
-    }
-
     public render() {
         return <div className="input-group">
-                <span className="input-group-btn">
-                    <button className="btn btn-info" type="button" onClick={() => this.props.onNumberDecrease()}>-</button>
-                </span>
-                <input type="text" value={this.props.value} className="form-control text-center" onChange={(e) => this.onNumberChange(e.target.value)} />
-                <span className="input-group-btn">
-                    <button className="btn btn-info" type="button" onClick={() => this.props.onNumberIncrease()}>+</button>
-                </span>
-            </div>;
+            <span className="input-group-btn">
+                <button className="btn btn-info" type="button" onClick={() => this.props.onNumberDecrease()}>-</button>
+            </span>
+            <input type="text" value={this.props.value} className="form-control text-center" onChange={(e) => this.onNumberChange(e.target.value)} />
+            <span className="input-group-btn">
+                <button className="btn btn-info" type="button" onClick={() => this.props.onNumberIncrease()}>+</button>
+            </span>
+        </div>;
+    }
+
+    private onNumberChange(value: any) {
+        this.props.onNumberChange(Number.isNaN(value) ? 0 : Number.parseInt(value));
     }
 }
