0ba219439b446f064beadb9e0c8b09bb868cf087
Kainer, Matthias <mail@matthias-kainer.de>
2017-04-30 23:01:20 +0200

    cleaned up SwitchProcessor.ts

--- public/javascript/src/game/compiler/processor/SwitchProcessor.ts
+++ public/javascript/src/game/compiler/processor/SwitchProcessor.ts
@@ -1,52 +1,44 @@
-import Machine from '../machine';
-import { SyntaxNode } from '../../../ast/node';
-import { SwitchComparisonNode, SwitchNode } from '../../../ast/availableNodes';
-import { NodeProcessor } from '../nodeProcessor';
+import Machine from "../machine";
+import { SyntaxNode } from "../../../ast/node";
+import { SwitchComparisonNode, SwitchNode } from "../../../ast/availableNodes";
+import { NodeProcessor } from "../nodeProcessor";
 
 export class SwitchProcessor implements NodeProcessor {
-    machine: Machine;
-
-    public constructor(machine: Machine) {
-        this.machine = machine;
-    }
+    public constructor(private machine: Machine) { }
 
     public canHandle(node: SyntaxNode): boolean {
         return node !== null && node.type === "SwitchNode";
     }
 
     public process(node: SyntaxNode): any {
-        let switchNode = node as SwitchNode;
         let statementResult: any = null;
-        if (switchNode.switches.find(_ => {
+        if ((node as SwitchNode).switches.find(_ => {
             _.parent = node;
             statementResult = this.machine.run(_);
             return statementResult !== SwitchComparisonProcessor.NORESULT;
-        })) return statementResult;
+        })) { return statementResult; }
 
         return null;
     }
 }
 
 export class SwitchComparisonProcessor implements NodeProcessor {
-    machine: Machine;
-    static NORESULT = "SwitchComparisonProcessor_NORESULT";
+    public static NORESULT = "SwitchComparisonProcessor_NORESULT";
 
-    public constructor(machine: Machine) {
-        this.machine = machine;
-    }
+    public constructor(private machine: Machine) { }
 
     public canHandle(node: SyntaxNode): boolean {
         return node !== null && node.type === "SwitchComparisonNode";
     }
 
     public process(node: SyntaxNode): any {
-        let comparisonNode = node as SwitchComparisonNode;
-        let switchNode = comparisonNode.parent as SwitchNode;
-        let match = this.machine.run(switchNode.matches);
-        let current = comparisonNode.comparable !== null ? this.machine.run(comparisonNode.comparable) : null;
+        const comparisonNode = node as SwitchComparisonNode;
+        const match = this.machine.run((comparisonNode.parent as SwitchNode).matches);
+        const current = comparisonNode.comparable !== null ? this.machine.run(comparisonNode.comparable) : null;
         // no matcher = default
-        if (current === null || match === current)
+        if (current === null || match === current) {
             return this.machine.run(comparisonNode.operation);
+        }
         return SwitchComparisonProcessor.NORESULT;
     }
 }
