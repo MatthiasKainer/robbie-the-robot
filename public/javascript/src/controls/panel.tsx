5ff492dc48a9e968111b2aeae7f328dda0d073f0
Kainer, Matthias <mail@matthias-kainer.de>
2017-04-30 22:34:26 +0200

    fix panel.tsx

--- public/javascript/src/controls/panel.tsx
+++ public/javascript/src/controls/panel.tsx
@@ -1,38 +1,37 @@
-declare var ace : any;
-import * as React from 'react';
-import { Action, ActionType, Direction, Event, GameState, Map, Robot, Way } from '../models';
-import HistoryList from './history';
-import DirectionControls from './directions';
-import Statistics from './statistics';
-import RobotProcessor from '../game/robot/processor';
+declare const ace: any;
+import * as React from "react";
+import { Action, ActionType, Direction, Event, GameState, Map, Robot, Way } from "../models";
+import HistoryList from "./history";
+import DirectionControls from "./directions";
+import Statistics from "./statistics";
+import RobotProcessor from "../game/robot/processor";
 
 interface PanelProperties {
     actions: Action[];
     gameState: GameState;
-    events : Event[];
-    map : Map;
-    way : Way;
-    robot : Robot;
+    events: Event[];
+    map: Map;
+    way: Way;
+    robot: Robot;
     onMove(action: Action): any;
     onRemoveStatement(index: number): any;
-    onChangeStatementCount(index: number, value : number) : void;
-    onUpdate(action : Action) : any;
+    onChangeStatementCount(index: number, value: number): void;
+    onUpdate(action: Action): any;
     onStart(): any;
 }
-
 
 export default class Panel extends React.Component<PanelProperties, any> {
     public constructor() {
         super();
-        this.state = { saved: false, code : "", editor : null };
+        this.state = { saved: false, code: "", editor: null };
     }
 
     public handleStart(event: any) {
         this.props.onStart();
     }
 
-    handleCodeChange(event : any) {
-        this.setState(Object.assign(this.state, {code: event.target.value}));
+    public handleCodeChange(event: any) {
+        this.setState(Object.assign(this.state, { code: event.target.value }));
     }
 
     public handleMove(movement: Action) {
@@ -53,58 +52,58 @@ export default class Panel extends React.Component<PanelProperties, any> {
             editor = ace.edit("editor");
             editor.setTheme("ace/theme/monokai");
             editor.getSession().setMode("ace/mode/robbielang");
-            this.setState(Object.assign(this.state, {editor: editor}));
+            this.setState(Object.assign(this.state, { editor }));
         }
     }
 
     public render() {
         console.log("Rendering Panel for map " + this.props.map.name);
 
-        let code_controls = <div id="editor" style={{ height: "300px" }} onChange={(e) => this.handleCodeChange(e)}></div>;
+        const codeControls = <div id="editor" style={{ height: "300px" }} onChange={(e) => this.handleCodeChange(e)}></div>;
 
-        let program_controls = this.props.way === Way.Click ? <div>
+        const programControls = this.props.way === Way.Click ? <div>
             <DirectionControls onMove={(direction) => this.handleMove(direction)} actions={this.props.map.actions} />
             <div className="play">
                 <button data-test="run program" className="btn-lg btn-primary btn-block fa fa-play" onClick={e => this.handleStart(e)}></button>
             </div>
-            </div> : 
-            <button data-test="run program" className="btn-lg btn-primary btn-block fa fa-play" 
+        </div> :
+            <button data-test="run program" className="btn-lg btn-primary btn-block fa fa-play"
                 onClick={e => {
                     /**
-                     * Calling this twice is unefficient, but required to 
+                     * Calling this twice is unefficient, but required to
                      * make cheating harder - users cannot overwrite move/robot position
                      * that easy (i.e. by `robot.position.row is 5`) when separating execution and interpretation
                      */
                     new RobotProcessor(this.props.map, this.props.robot)
                         .runCode(this.state.editor.getValue(), {
-                            [ActionType[ActionType.Movement]] : (_) => this.props.onUpdate(_),
-                            [ActionType[ActionType.Dig]] : (_) => this.props.onUpdate(_)
+                            [ActionType[ActionType.Movement]]: (_) => this.props.onUpdate(_),
+                            [ActionType[ActionType.Dig]]: (_) => this.props.onUpdate(_),
                         });
                     this.props.onStart();
-                    }
+                }
                 }></button>;
 
-        let restart_controls = <div>
-            <button 
-                className="btn-lg btn-primary btn-block fa fa-refresh" 
+        const restartControls = <div>
+            <button
+                className="btn-lg btn-primary btn-block fa fa-refresh"
                 onClick={(e: any) => location.reload()}>
             </button>
             <Statistics events={this.props.events} map={this.props.map} gameState={this.props.gameState} />
         </div>;
 
-        let history = this.props.way === Way.Click ? <HistoryList 
-                events={this.props.events}
-                gameState={this.props.gameState}
-                onChangeStatementCount={(index, value) => this.props.onChangeStatementCount(index, value)}
-                onRemoveStatement={(index) => this.props.onRemoveStatement(index)} 
-                actions={this.props.actions} /> : <div />
+        const history = this.props.way === Way.Click ? <HistoryList
+            events={this.props.events}
+            gameState={this.props.gameState}
+            onChangeStatementCount={(index, value) => this.props.onChangeStatementCount(index, value)}
+            onRemoveStatement={(index) => this.props.onRemoveStatement(index)}
+            actions={this.props.actions} /> : <div />;
 
         return <div className="controls">
             <input type="hidden" data-test="game state" value={GameState[this.props.gameState]} />
-            {this.props.way === Way.Code ? code_controls : ""}
-            {this.props.gameState === GameState.STOP ? program_controls : ""}
-            {this.props.gameState === GameState.LOOSE ? restart_controls : ""}
-            {this.props.gameState === GameState.WIN ? restart_controls : ""}
+            {this.props.way === Way.Code ? codeControls : ""}
+            {this.props.gameState === GameState.STOP ? programControls : ""}
+            {this.props.gameState === GameState.LOOSE ? restartControls : ""}
+            {this.props.gameState === GameState.WIN ? restartControls : ""}
             {history}
         </div>;
     }
