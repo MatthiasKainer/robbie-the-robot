830be2477f92f89f57bb9e417abc421515d1a302
Kainer, Matthias <mail@matthias-kainer.de>
2017-05-01 10:25:57 +0200

    cleaned up tutorial-app.tsx

--- public/javascript/src/tutorial-app.tsx
+++ public/javascript/src/tutorial-app.tsx
@@ -1,6 +1,6 @@
-import { Tutorial } from './game/tutorial/intro';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { Tutorial } from "./game/tutorial/intro";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 export default class App extends React.Component<any, any> {
     public render() {
         return <Tutorial />;
