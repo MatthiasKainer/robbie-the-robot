cc2fe18cb66ea11e484cecb50cbdf8cf568233ad
Kainer, Matthias <mail@matthias-kainer.de>
2017-05-01 10:19:11 +0200

    cleaned up imagePreloader.tsx

--- public/javascript/src/imagePreloader.tsx
+++ public/javascript/src/imagePreloader.tsx
@@ -1,34 +1,34 @@
-import { ApiGateway, Routes } from './net/api';
-import * as React from 'react';
+import { ApiGateway, Routes } from "./net/api";
+import * as React from "react";
 
-let images = [
-    "/images"
-]
+const images = [
+    "/images",
+];
 
 interface ImageLoaderState {
-    images : string[]
+    images: string[];
 }
 
 export class ImageLoader extends React.Component<any, ImageLoaderState> {
     public constructor() {
         super();
-        this.state = { images : []};
+        this.state = { images: [] };
     }
 
     public componentWillMount() {
         ApiGateway.get(new Routes.Images()).then(_ => {
-            this.setState({ images : _.images })
-        })
+            this.setState({ images: _.images });
+        });
     }
 
     public render() {
-        let images : any[] = [];
+        const images: any[] = [];
         this.state.images.forEach(_ => {
             images.push(<img key={_} src={`/images/${_}`} />);
         });
 
-        return <div style={{position: "absolute", top: "0", left:"0", width:"1", height:"1", overflow: "hidden"}}>
+        return <div style={{ position: "absolute", top: "0", left: "0", width: "1", height: "1", overflow: "hidden" }}>
             {images}
-            </div>;
+        </div>;
     }
 }
