89a1092c8e7b009d01a75b3d07599e5986275352
Kainer, Matthias <mail@matthias-kainer.de>
2017-05-01 10:28:45 +0200

    cleaned up images.ts

--- routes/src/images.ts
+++ routes/src/images.ts
@@ -1,40 +1,39 @@
-import * as express from 'express';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as express from "express";
+import * as fs from "fs";
+import * as path from "path";
 export let router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
     res.type("application/json");
     new ImageFilesReader().all()
         .then((images) => {
             res.send({
-                images : images
+                images,
             });
         })
         .catch((err) => {
             console.error(err);
             res.sendStatus(500);
-        })
+        });
 });
 
-let extensions = {
-    image: ['.png', '.jpg']
+const extensions = {
+    image: [".png", ".jpg"],
 };
 
-let is = {
+const is = {
     image: (element: string) => {
-        var extName = path.extname(element);
-        return extensions.image.indexOf(extName) >= 0;
-    }
+        return extensions.image.indexOf(path.extname(element)) >= 0;
+    },
 };
 
 export class ImageFilesReader {
-    static images: string = __dirname + "/../../public/images";
+    public static images: string = __dirname + "/../../public/images";
 
     public all(): Promise<any> {
         return new Promise((resolve, reject) => {
             fs.readdir(ImageFilesReader.images, (err, files) => {
-                if (err) return reject(err);
+                if (err) { return reject(err); }
 
                 return resolve(files
                     .filter(is.image));
