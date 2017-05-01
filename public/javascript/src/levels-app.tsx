c24c7553872ca9e586692990ea3b0e97bcb4b825
Kainer, Matthias <mail@matthias-kainer.de>
2017-05-01 10:19:53 +0200

    cleaned up levels-app.tsx

--- public/javascript/src/levels-app.tsx
+++ public/javascript/src/levels-app.tsx
@@ -1,6 +1,6 @@
-import { LevelSelector } from './game/levels';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { LevelSelector } from "./game/levels";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 export default class App extends React.Component<any, any> {
     public render() {
