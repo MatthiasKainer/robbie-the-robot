b101fd28566e232f254299a9c8e4707a08c1c558
Kainer, Matthias <mail@matthias-kainer.de>
2017-05-01 11:31:57 +0200

    cleaned up testComparingProcessor.ts

--- test/public/javascript/src/game/compiler/testComparingProcessor.ts
+++ test/public/javascript/src/game/compiler/testComparingProcessor.ts
@@ -1,62 +1,61 @@
-import { Comparator, Operator, SyntaxNode } from '../../../../../../public/javascript/src/ast/node';
-import Machine from '../../../../../../public/javascript/src/game/compiler/machine';
-import mocha = require('mocha');
+import { Comparator, Operator, SyntaxNode } from "../../../../../../public/javascript/src/ast/node";
+import Machine from "../../../../../../public/javascript/src/game/compiler/machine";
+import mocha = require("mocha");
 import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
-import chai = require('chai');
-import * as sinon from 'sinon';
+import chai = require("chai");
+import * as sinon from "sinon";
 import {
-    ComparingProcessor
-} from './../../../../../../public/javascript/src/game/compiler/availableNodeProcessors';
+    ComparingProcessor,
+} from "./../../../../../../public/javascript/src/game/compiler/availableNodeProcessors";
 import {
     AnyValueNode,
     ComparingNode,
-    StringNode
-} from './../../../../../../public/javascript/src/ast/availableNodes';
+    StringNode,
+} from "./../../../../../../public/javascript/src/ast/availableNodes";
 const expect = chai.expect;
 
-let name = "name";
-let node = new ComparingNode(Comparator.Equal, new StringNode(name), new StringNode(name));
+const name = "name";
+const node = new ComparingNode(Comparator.Equal, new StringNode(name), new StringNode(name));
 
 @suite("[ComparingProcessor] When requesting a node")
 class CanHandle {
-    machine: Machine;
-    processor: ComparingProcessor;
+    private machine: Machine;
+    private processor: ComparingProcessor;
 
-    before() {
+    public before() {
         this.machine = new Machine();
         this.processor = new ComparingProcessor(this.machine);
     }
 
-    @test "it can handle, it should have responded with can handle"() {
-        let result = this.processor.canHandle(node);
+    @test public "it can handle, it should have responded with can handle"() {
+        const result = this.processor.canHandle(node);
         expect(result).to.be.true;
     }
 
-    @test "it's not a ClassNode handle, it should have responded with cannot handle"() {
-        let node = new AnyValueNode("class");
-        let result = this.processor.canHandle(node);
+    @test public "it's not a ClassNode handle, it should have responded with cannot handle"() {
+        const node = new AnyValueNode("class");
+        const result = this.processor.canHandle(node);
         expect(result).to.be.false;
     }
 
-    @test "it's null, it should have responded with cannot handle"() {
-        let node = null;
-        let result = this.processor.canHandle(node);
+    @test public "it's null, it should have responded with cannot handle"() {
+        const result = this.processor.canHandle(null);
         expect(result).to.be.false;
     }
 }
 
 @suite("[ComparingProcessor] When requesting a node from the processor")
 class Process {
-    machine: Machine;
-    processor: ComparingProcessor;
+    private machine: Machine;
+    private processor: ComparingProcessor;
 
-    before() {
+    public before() {
         this.machine = new Machine();
         this.processor = new ComparingProcessor(this.machine);
     }
 
-    @test "an equal comparison"() {
-        let result = this.processor.process(node);
+    @test public "an equal comparison"() {
+        const result = this.processor.process(node);
         expect(result).not.undefined;
         expect(result).not.null;
         expect(result).to.true;
