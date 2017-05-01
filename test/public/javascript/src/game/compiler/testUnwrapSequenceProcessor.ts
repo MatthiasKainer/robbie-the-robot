14bca9795a9e20c387b62b85a463b695db6f989b
Kainer, Matthias <mail@matthias-kainer.de>
2017-05-01 11:52:19 +0200

    cleaned up testUnwrapSequenceProcessor.ts

--- test/public/javascript/src/game/compiler/testUnwrapSequenceProcessor.ts
+++ test/public/javascript/src/game/compiler/testUnwrapSequenceProcessor.ts
@@ -1,12 +1,12 @@
-import Machine from '../../../../../../public/javascript/src/game/compiler/machine';
-import mocha = require('mocha');
+import Machine from "../../../../../../public/javascript/src/game/compiler/machine";
+import mocha = require("mocha");
 import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
-import chai = require('chai');
-import * as sinon from 'sinon';
+import chai = require("chai");
+import * as sinon from "sinon";
 import {
     LoopProcessor,
-    UnwrapSequenceProcessor
-} from './../../../../../../public/javascript/src/game/compiler/availableNodeProcessors';
+    UnwrapSequenceProcessor,
+} from "./../../../../../../public/javascript/src/game/compiler/availableNodeProcessors";
 import {
     AnyValueNode,
     AssignmentNode,
@@ -16,70 +16,69 @@ import {
     SequenceNode,
     StringNode,
     UnwrapSequenceNode,
-    VariableNode
-} from './../../../../../../public/javascript/src/ast/availableNodes';
+    VariableNode,
+} from "./../../../../../../public/javascript/src/ast/availableNodes";
 const expect = chai.expect;
 
 @suite("[UnwrapSequenceProcessor] When requesting a node from the processor")
 class CanHandle {
-    machine : Machine;
-    processor : UnwrapSequenceProcessor;
+    public machine: Machine;
+    public processor: UnwrapSequenceProcessor;
 
-    before() {
+    public before() {
         this.machine = new Machine();
         this.processor = new UnwrapSequenceProcessor(this.machine);
     }
 
-    @test "that it can handle, it should have responded with can handle"() {
-        let node = new UnwrapSequenceNode();
-        let result = this.processor.canHandle(node);
+    @test public "that it can handle, it should have responded with can handle"() {
+        const node = new UnwrapSequenceNode();
+        const result = this.processor.canHandle(node);
         expect(result).to.be.true;
     }
 
-    @test "that's not a SequenceNode, it should have responded with cannot handle"() {
-        let node = new AnyValueNode("class");
-        let result = this.processor.canHandle(node);
+    @test public "that's not a SequenceNode, it should have responded with cannot handle"() {
+        const node = new AnyValueNode("class");
+        const result = this.processor.canHandle(node);
         expect(result).to.be.false;
     }
 
-    @test "that's null, it should have responded with cannot handle"() {
-        let node = null;
-        let result = this.processor.canHandle(node);
+    @test public "that's null, it should have responded with cannot handle"() {
+        const result = this.processor.canHandle(null);
         expect(result).to.be.false;
     }
 }
 
 @suite("[UnwrapSequenceProcessor] When requesting a node from the class processor")
 class Process {
-    machine : Machine;
-    processor : UnwrapSequenceProcessor;
+    public machine: Machine;
+    public processor: UnwrapSequenceProcessor;
 
-    before() {
+    public before() {
         this.machine = new Machine();
         this.processor = new UnwrapSequenceProcessor(this.machine);
     }
 
-    @test "a perfect sequence"() {
-        
-        let positionData = new ClassNode("Position", {
+    @test public "a perfect sequence"() {
+
+        const positionData = new ClassNode("Position", {
             "row": new NumberNode(5),
-            "column": new NumberNode(6)
+            "column": new NumberNode(6),
         });
 
-        let robotData = new ClassNode("Robot", {
-            "position": positionData
+        const robotData = new ClassNode("Robot", {
+            "position": positionData,
         });
-        
-        let robotVariable = new VariableNode(new StringNode("robot"), new StringNode("Robot"));
-        let assignedRobot = new AssignmentNode(new StringNode("robot"), robotData);
 
-        let row = new UnwrapSequenceNode(new ExpandVariableNode(new StringNode("robot")),
+        const robotVariable = new VariableNode(new StringNode("robot"), new StringNode("Robot"));
+        const assignedRobot = new AssignmentNode(new StringNode("robot"), robotData);
+
+        const row = new UnwrapSequenceNode(new ExpandVariableNode(new StringNode("robot")),
             new ExpandVariableNode(new StringNode("position")),
             new ExpandVariableNode(new StringNode("row")));
-        let column = new UnwrapSequenceNode(new ExpandVariableNode(new StringNode("robot")),
+        const column = new UnwrapSequenceNode(new ExpandVariableNode(new StringNode("robot")),
             new ExpandVariableNode(new StringNode("position")),
             new ExpandVariableNode(new StringNode("column")));
-        let results = this.machine.run(new SequenceNode(robotVariable, assignedRobot, row, column));
+        const results = this.machine.run(new SequenceNode(robotVariable, assignedRobot, row, column));
 
         expect(results).to.deep.eq([true, true, 5, 6]);
     }
