8fd7ed930d9c7a3a1000b681993290e0070be165
Kainer, Matthias <mail@matthias-kainer.de>
2017-05-01 10:26:39 +0200

    cleaned up editing.ts

--- routes/src/editing.ts
+++ routes/src/editing.ts
@@ -1,11 +1,11 @@
-import { Way } from '../../public/javascript/src/models';
-import { editingMode } from '../utils/cookies';
-import * as express from 'express';
+import { Way } from "../../public/javascript/src/models";
+import { editingMode } from "../utils/cookies";
+import * as express from "express";
 export let router = express.Router();
 
-router.get('/mode', (req, res, next) => {
+router.get("/mode", (req, res, next) => {
     res.type("application/json");
     res.send({
-        way : req.cookies[editingMode] || Way.Click
+        way : req.cookies[editingMode] || Way.Click,
     });
 });
