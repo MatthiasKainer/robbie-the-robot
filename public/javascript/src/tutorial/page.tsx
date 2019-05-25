812b2c6c816ec647acbe27038555cf5a36ab2772
Matthias Kainer <mail@matthias-kainer.de>
2019-05-25 16:50:58 +0200

    Starting tutorial only on demand (on click)

--- public/javascript/src/tutorial/page.tsx
+++ public/javascript/src/tutorial/page.tsx
@@ -17,23 +17,29 @@ interface PageProps {
     elements: PageElement[];
 }
 
-export class Page extends React.Component<PageProps, PageProps> {
+interface PageState {
+    started: boolean
+}
+
+export class Page extends React.Component<PageProps, PageProps & PageState> {
     timeouts: NodeJS.Timer[];
     audio: HTMLAudioElement;
 
     public constructor(props: PageProps) {
         super(props);
         this.timeouts = [];
-        this.state = Object.assign({}, props);
+        this.state = Object.assign({ started: false }, props);
     }
 
     playAudio(file: string) {
         if (!file) return;
         try {
+            console.log(`[Tutorial] Playing /audio/en/${file}.`)
+
             this.audio = new Audio(`/audio/en/${file}`);
             this.audio.play();
         } catch (e) {
-            console.log("[Tutorial] Could not play audio. Hopefully people get the idea from the overlays.")
+            console.log("[Tutorial] Could not play audio. Hopefully people get the idea from the overlays.", e)
         }
     }
 
@@ -61,17 +67,15 @@ export class Page extends React.Component<PageProps, PageProps> {
         this.timeouts.push(setTimeout(() => {
             this.state.elements.splice(0, 1)[0];
             console.log(`[Tutorial/Page] Applying ${JSON.stringify(currentElement)}`);
-            this.setState(Object.assign({}, this.state));
+            this.setState({...this.state});
         }, this.state.elements[0].duration + 1000));
     }
 
-    checkIfAbort(event: KeyboardEvent) {
-        if (event.keyCode === 27) {
-            this.setState(Object.assign({}, { elements: [] }));
-            if (this.audio) {
-                this.audio.pause();
-                this.audio.remove();
-            }
+    checkIfAbort() {
+        this.setState(Object.assign({}, { elements: [] }));
+        if (this.audio) {
+            this.audio.pause();
+            this.audio.remove();
         }
     }
 
@@ -80,34 +84,50 @@ export class Page extends React.Component<PageProps, PageProps> {
     }
 
     componentDidMount() {
-        this.incrementState();
-        document.addEventListener("keydown", (e) => this.checkIfAbort(e), false);
+        document.addEventListener("keydown", () => this.checkIfAbort(), false);
     }
 
     componentWillUnmount() {
-        document.removeEventListener("keydown", (e : any) => this.checkIfAbort(e), false);
+        document.removeEventListener("keydown", () => this.checkIfAbort(), false);
+        document.removeEventListener("mousedown", () => this.checkIfAbort(), false);
     }
 
     componentDidUpdate() {
-        this.incrementState();
+        if (this.state.started) {
+            this.incrementState();
+        }
+    }
+
+    startTutorial() {
+        console.log("Starting tutorials...");
+        this.setState({ ...this.state, started: true });
     }
 
     public render() {
         const current = this.state.elements[0];
         return this.state.elements.length < 1
-            ? null :
-            <div data-test="tutorial-page" style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                background: "black",
-                opacity: 0.5,
-                zIndex: 10000
-            }} >
-                <div className="controls"></div>
-                <Element target={current.target} />
-            </div>;
+            ? <div></div> :
+            <div>{this.state.started
+                ? <div data-test="tutorial-page" style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    background: "black",
+                    opacity: 0.5,
+                    zIndex: 10000
+                }} >
+                    <div className="controls"></div>
+                    <Element target={current.target} />
+                </div> 
+                : <div data-test="start-tutorial" className="fa fa-question-circle" style={{
+                    fontSize: "4rem",
+                    position: "absolute",
+                    right: 10,
+                    bottom: 10,
+                    cursor: "pointer",
+                    zIndex: 1000
+                }} onClick={() => this.startTutorial()}></div>}</div>;
     }
 }
--- test/public/javascript/src/tutorial/testPage.tsx
+++ test/public/javascript/src/tutorial/testPage.tsx
@@ -8,6 +8,12 @@ import { suite, test } from "mocha-typescript";
 import Element from "../../../../../public/javascript/src/tutorial/element";
 import { Page, PageElement } from "../../../../../public/javascript/src/tutorial/page";
 
+class Audio {
+    play() {}
+}
+
+(global as any).Audio = Audio;
+
 @suite("[Page] When having no elements")
 class Empty {
     page: ReactWrapper<Page, any>;
@@ -34,13 +40,14 @@ class One {
         this.elements = [{
             duration: 500,
             target: this.target,
-            audio: ""
+            audio: "file"
         }];
         this.clock = sinon.useFakeTimers();
         this.spies = [
             sinon.spy(Page.prototype, 'playAudio')
         ];
         this.page = mount(<Page elements={this.elements} />);
+        (this.page.instance() as any).startTutorial()
     }
 
     public after() {
@@ -49,14 +56,14 @@ class One {
     }
 
     @test public "it should play the element and hide the tutorial"() {
-        expect(this.page.contains(<Element target={this.target} />)).to.be.true;
+        expect(this.page.contains(<Element target={this.target} />), "Element added").to.be.true;
         expect(this.page.find(`[data-test="tutorial-page"]`).length).to.equal(1);
-        expect((Page.prototype.playAudio as sinon.SinonSpy).called).to.equal(false);
+        expect((Page.prototype.playAudio as sinon.SinonSpy).called, "Audio not started").to.equal(false);
         this.clock.tick(600);
-        expect((Page.prototype.playAudio as sinon.SinonSpy).calledOnce).to.equal(true);
+        expect((Page.prototype.playAudio as sinon.SinonSpy).callCount, "Audio started").to.equal(1);
         this.clock.tick(2000);
-        expect((Page.prototype.playAudio as sinon.SinonSpy).calledOnce).to.equal(true);
-        expect(this.page.contains(<Element target={this.target} />)).to.be.false;
+        expect((Page.prototype.playAudio as sinon.SinonSpy).calledOnce, "Audio started").to.equal(true);
+        expect(this.page.contains(<Element target={this.target} />), "Element removed").to.be.false;
         expect(this.page.find(`[data-test="tutorial-page"]`).length).to.equal(0);
     }
 }
@@ -74,17 +81,18 @@ class Multiple {
         this.elements = [{
             duration: 500,
             target: this.target + "1",
-            audio: ""
+            audio: "file"
         }, {
             duration: 500,
             target: this.target + "2",
-            audio: ""
+            audio: "file"
         }];
         this.clock = sinon.useFakeTimers();
         this.spies = [
             sinon.spy(Page.prototype, 'playAudio')
         ];
         this.page = mount(<Page elements={this.elements} />);
+        (this.page.instance() as any).startTutorial()
     }
 
     public after() {
@@ -93,17 +101,17 @@ class Multiple {
     }
 
     @test public "it should play all elements and hide the tutorial"() {
-        expect(this.page.contains(<Element target={this.target + "1"} />)).to.be.true;
+        expect(this.page.contains(<Element target={this.target + "1"} />), "First element expected").to.be.true;
         expect(this.page.find(`[data-test="tutorial-page"]`).length).to.equal(1);
-        expect((Page.prototype.playAudio as sinon.SinonSpy).calledOnce).to.equal(false);
+        expect((Page.prototype.playAudio as sinon.SinonSpy).calledOnce, "Audio not yet started").to.equal(false);
         this.clock.tick(600);
-        expect((Page.prototype.playAudio as sinon.SinonSpy).calledOnce).to.equal(true);
+        expect((Page.prototype.playAudio as sinon.SinonSpy).callCount, "First Audio started").to.equal(1);
         this.clock.tick(1000);
-        expect(this.page.contains(<Element target={this.target + "2"} />)).to.be.true;
+        expect(this.page.contains(<Element target={this.target + "2"} />), "Second element expected").to.be.true;
         expect(this.page.find(`[data-test="tutorial-page"]`).length).to.equal(1);
         this.clock.tick(2000);
-        expect((Page.prototype.playAudio as sinon.SinonSpy).calledTwice).to.equal(true);
-        expect(this.page.contains(<Element target={this.target + "2"} />)).to.be.false;
+        expect((Page.prototype.playAudio as sinon.SinonSpy).calledTwice, "Second audi started").to.equal(true);
+        expect(this.page.contains(<Element target={this.target + "2"} />), "Second element gone").to.be.false;
         expect(this.page.find(`[data-test="tutorial-page"]`).length).to.equal(0);
     }
 }
